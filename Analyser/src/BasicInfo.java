624d3cc310a846757ec86e4f4ced3915be9c9b0e
Yu Ting <youthinkk@outlook.com>
2016-04-13 14:40:36 +0800

    Determine if the module is under science approved list

--- Analyser/src/BasicInfo.java
+++ Analyser/src/BasicInfo.java
@@ -29,6 +29,7 @@ public class BasicInfo {
 			String type = "";
 			boolean isGem = false;
 			boolean isSS = false;
+			boolean isScience = false;
 			
 			if (input.trim().isEmpty()) continue;
 			
@@ -45,6 +46,8 @@ public class BasicInfo {
 					
 					if (matcher2.group(1).contains("SS")) {
 						isSS = true;
+					} else if (isScience(code)) {
+						isScience = true;
 					}
 				}
 				
@@ -73,6 +76,8 @@ public class BasicInfo {
 				System.out.println("GEM");
 			} else if (isSS) {
 				System.out.println("SS");
+			} else if (isScience) {
+				System.out.println("Science");
 			} else {
 				System.out.println("Module");
 			}
@@ -81,6 +86,41 @@ public class BasicInfo {
 		}
 		
 		scanner.close();
+	}
+	
+	private boolean isScience(String input) {
+		return input.equals("CM1121") ||
+				input.equals("CM1131") ||
+				input.equals("CM1417") ||
+				input.equals("LSM1301") ||
+				input.equals("LSM1302") ||
+				input.equals("PC1141") ||
+				input.equals("PC1142") ||
+				input.equals("PC1143") ||
+				input.equals("PC1144") ||
+				input.equals("PC1221") ||
+				input.equals("PC1222") ||
+				input.equals("PC1432") ||
+				input.equals("MA2213") ||
+				input.equals("MA2214") ||
+				input.equals("CM1101") ||
+				input.equals("CM1111") ||
+				input.equals("CM1161") ||
+				input.equals("CM1191") ||
+				input.equals("CM1401") ||
+				input.equals("CM1402") ||
+				input.equals("CM1501") ||
+				input.equals("CM1502") ||
+				input.equals("LSM1303") ||
+				input.equals("PC1421") ||
+				input.equals("PC1431") ||
+				input.equals("PC1433") ||
+				input.equals("MA1104") ||
+				input.equals("MA2101") ||
+				input.equals("MA2108") ||
+				input.equals("MA2501") ||
+				input.equals("ST2132") ||
+				input.equals("ST2137");
 	}
 	
 
